e05035bd78f06887c540349dc88261bf936693f5
Matteo Bolognini <90386956+matteo-bolognini@users.noreply.github.com>
2024-12-12 17:15:22 +0000

    Update JamfConnect_LogStream_Script.sh

--- scripts/bash/Jamf Connect Privilege Elevation LogStream/JamfConnect_LogStream_Script.sh
+++ scripts/bash/Jamf Connect Privilege Elevation LogStream/JamfConnect_LogStream_Script.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
+# Set the directory and file path
+SCRIPT_DIR="/Library/com.my_org.com"
+SCRIPT_PATH="$SCRIPT_DIR/my_script.sh"
+
+# Create the directory if it doesn't exist
+if [ ! -d "$SCRIPT_DIR" ]; then
+    echo "Creating directory $SCRIPT_DIR"
+    sudo mkdir -p "$SCRIPT_DIR"
+fi
+
+# Create the script file
+cat << 'EOF' | sudo tee "$SCRIPT_PATH" > /dev/null
+#!/bin/bash
+
 # Set the path where the log files will be saved
-path="/tmp/logs"
+path="/Library/com.my_org.com/logs"
 
 # Check if the directory exists
 if [ ! -d "$path" ]; then
-	echo "Directory $path does not exist. Creating it now..."
-	mkdir -p "$path"
-	if [ $? -eq 0 ]; then
-		echo "Directory created successfully."
-	else
-		echo "Failed to create directory. Please check permissions and path."
-		exit 1
-	fi
+    echo "Directory $path does not exist. Creating it now..."
+    mkdir -p "$path"
+    if [ $? -eq 0 ]; then
+        echo "Directory created successfully."
+    else
+        echo "Failed to create directory. Please check permissions and path."
+        exit 1
+    fi
 else
-	echo "Directory $path already exists."
+    echo "Directory $path already exists."
 fi
 
 # Collect Jamf Connect Daemon Elevation Logs
@@ -26,3 +40,12 @@ echo "Collecting Jamf Connect Menubar Elevation Logs..."
 log show --debug --info --style compact --predicate '(subsystem == "com.jamf.connect") && (category == "PrivilegeElevation")' >> "$path/Connect_Menubar_Elevation_Logs.txt"
 
 echo "Log collection complete. Files saved in $path"
+EOF
+
+# Set the correct permissions
+chmod 755 "$SCRIPT_PATH"
+
+# Make the script executable
+chmod +x "$SCRIPT_PATH"
+
+echo "Script has been created at $SCRIPT_PATH and made executable"
